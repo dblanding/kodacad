8e607cc6d01231296923f0c882c24fe472de1cf1
Doug Blanding <dblanding@gmail.com>
2020-03-19 12:36:17 -0400

    Revised Bottle Tutorial to use pull for the neck. Change to version '0.1.2'

--- version.py
+++ version.py
@@ -1,2 +1,3 @@
-APP_VERSION = "0.1.1"  # 2/24/2020 'Round trip' Load/Save STEP works crudely (save all parts in _partDict)
+APP_VERSION = "0.1.2"  # 3/19/2020 OCAF document format adopted for CAD data model
+# APP_VERSION = "0.1.1"  # 2/24/2020 'Round trip' Load/Save STEP works crudely (save all parts in _partDict)
 # APP_VERSION = "0.1.0"  # 2/17/2020 Initial release
