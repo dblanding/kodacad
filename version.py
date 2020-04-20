f1e68cb77abd25c68634d5c0601de0404b027a84
Doug Blanding <dblanding@gmail.com>
2020-04-20 19:14:12 -0400

    Version change to "0.2.2"

--- version.py
+++ version.py
@@ -1,4 +1,5 @@
-APP_VERSION = "0.2.1"  # 4/10/2020 Able to modify a shape and have all shared instances display correctly.
+APP_VERSION = "0.2.2"  # 4/20/2020 Incremental draw/hide of shapes and use hide_list instead of draw_list.
+# APP_VERSION = "0.2.1"  # 4/10/2020 Able to modify a shape and have all shared instances display correctly.
 # APP_VERSION = "0.2.0"  # 4/8/2020 Merged dev branch which uses 'entry' based uid's (rather than integers).
 # APP_VERSION = "0.1.2"  # 3/19/2020 OCAF document format adopted for CAD data model
 # APP_VERSION = "0.1.1"  # 2/24/2020 'Round trip' Load/Save STEP works crudely (save all parts in _partDict)
